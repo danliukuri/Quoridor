6285e0af8a8fd59b8aac7b0bf240fb26e19a52c9
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-09-30 20:36:27 +0100

    Add an input system with some hints

---
+++ Quoridor.Console.App/ConsoleInput.cs
@@ -0,0 +1,225 @@
+﻿using Quoridor.Controllers;
+using Quoridor.Models;
+using System;
+
+namespace Quoridor.Console.App
+{
+    class ConsoleInput
+    {
+        public void StartProcessing(QuoridorController quoridorPvPController, PlayerController playerPvPController,
+            QuoridorController quoridorPvAIController, PlayerController playerPvAIController,
+            WeakAIController weakAIController)
+        {
+            ConsoleOutputForInput.OutputGameTitle();
+            bool finishProcessing = false; ;
+            while (!finishProcessing)
+            {
+                if (quoridorPvPController.QuoridorModel.IsGameEnded || quoridorPvAIController.QuoridorModel.IsGameEnded)
+                {
+                    ConsoleOutputForInput.OutputGameTitle();
+                    quoridorPvPController.QuoridorModel.Reset();
+                    quoridorPvAIController.QuoridorModel.Reset();
+                }
+
+                ConsoleOutputForInput.InputView();
+                string command = System.Console.ReadLine();
+                var splitCommand = command.Split(new char[0]);
+                switch (splitCommand[0].ToLower())
+                {
+                    case "pvp":
+                        StartProcessingPvP(quoridorPvPController, playerPvPController);
+                        break;
+                    case "pvai":
+                        StartProcessingPvAI(quoridorPvAIController, playerPvAIController, weakAIController);
+                        break;
+                    case "quit":
+                        finishProcessing = true;
+                        break;
+                    default:
+                        ConsoleOutputForInput.UnknownCommand();
+                        break;
+                }
+            }
+        }
+
+        public void StartProcessingPvP(QuoridorController quoridorController, PlayerController playerController)
+        {
+            string command;
+            ConsoleOutputForInput.OutputPvPModeTitle();
+            quoridorController.StartGame();
+            
+            while (!quoridorController.QuoridorModel.IsGameEnded)
+            {
+                ConsoleOutputForInput.PlayerInputView(quoridorController.QuoridorModel.CurrentPlayer);
+                command = System.Console.ReadLine();
+                string[] splitCommand = command.Split(new char[0]);
+                try
+                {
+                    switch (splitCommand[0].ToLower())
+                    {
+                        case "move":
+                            {
+                                Direction direction = splitCommand[1].ToLower() switch
+                                {
+                                    "up" => Direction.Up,
+                                    "down" => Direction.Down,
+                                    "left" => Direction.Left,
+                                    "right" => Direction.Right,
+                                    _ => throw new ArgumentException($"There is no such direction ({splitCommand[1]})")
+                                };
+                                if (splitCommand.Length == 3)
+                                {
+                                    Direction directionFromAnotherPlayer = splitCommand[2].ToLower() switch
+                                    {
+                                        "up" => Direction.Up,
+                                        "down" => Direction.Down,
+                                        "left" => Direction.Left,
+                                        "right" => Direction.Right,
+                                        _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
+                                    };
+                                    playerController.MakePlayerMove(direction, directionFromAnotherPlayer);
+                                }
+                                else
+                                    playerController.MakePlayerMove(direction);
+                            }
+                            break;
+                        case "place":
+                            {
+                                if (splitCommand[1].ToLower() != "wall")
+                                {
+                                    throw new ArgumentException($"\nUnknown command. Can't place '{splitCommand[1].ToLower()}'");
+                                }
+
+                                Direction direction = splitCommand[2].ToLower() switch
+                                {
+                                    "up" => Direction.Up,
+                                    "down" => Direction.Down,
+                                    "left" => Direction.Left,
+                                    "right" => Direction.Right,
+                                    _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
+                                };
+                                int w = int.Parse(splitCommand[3]) - 1;
+                                int h = int.Parse(splitCommand[4]) - 1;
+                                playerController.PlaceWall(direction, w, h);
+                            }
+                            break;
+                        case "quit":
+                            {
+                                ConsoleOutputForInput.QuitTheGame(quoridorController.QuoridorModel.CurrentPlayer);
+                                quoridorController.QuoridorModel.IsGameEnded = true;
+                                break;
+                            }
+                        default:
+                            ConsoleOutputForInput.UnknownCommand();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ConsoleOutputForInput.Exception(ex.Message);
+                }
+            }
+        }
+
+        public void StartProcessingPvAI(QuoridorController quoridorController, PlayerController playerController,
+            WeakAIController weakAIController)
+        {
+            string command;
+            ConsoleOutputForInput.OutputPvAIModeTitle();
+            quoridorController.StartGame();
+
+            bool isPlayerTurn = true;
+            while (!quoridorController.QuoridorModel.IsGameEnded)
+            { 
+                if (isPlayerTurn)
+                {
+                    ConsoleOutputForInput.PlayerInputView(quoridorController.QuoridorModel.CurrentPlayer);
+                    command = System.Console.ReadLine();
+                    string[] splitCommand = command.Split(new char[0]);
+                    try
+                    {
+                        switch (splitCommand[0].ToLower())
+                        {
+                            case "move":
+                                {
+                                    Direction direction = splitCommand[1].ToLower() switch
+                                    {
+                                        "up" => Direction.Up,
+                                        "down" => Direction.Down,
+                                        "left" => Direction.Left,
+                                        "right" => Direction.Right,
+                                        _ => throw new ArgumentException($"There is no such direction ({splitCommand[1]})")
+                                    };
+                                    if (splitCommand.Length == 3)
+                                    {
+                                        Direction directionFromAnotherPlayer = splitCommand[2].ToLower() switch
+                                        {
+                                            "up" => Direction.Up,
+                                            "down" => Direction.Down,
+                                            "left" => Direction.Left,
+                                            "right" => Direction.Right,
+                                            _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
+                                        };
+                                        playerController.MakePlayerMove(direction, directionFromAnotherPlayer);
+                                    }
+                                    else
+                                        playerController.MakePlayerMove(direction);
+                                    isPlayerTurn = false;
+                                }
+                                break;
+                            case "place":
+                                {
+                                    if (splitCommand[1].ToLower() != "wall")
+                                    {
+                                        throw new ArgumentException($"\nUnknown command. Can't place '{splitCommand[1].ToLower()}'");
+                                    }
+
+                                    Direction direction = splitCommand[2].ToLower() switch
+                                    {
+                                        "up" => Direction.Up,
+                                        "down" => Direction.Down,
+                                        "left" => Direction.Left,
+                                        "right" => Direction.Right,
+                                        _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
+                                    };
+                                    int w = int.Parse(splitCommand[3]) - 1;
+                                    int h = int.Parse(splitCommand[4]) - 1;
+                                    playerController.PlaceWall(direction, w, h);
+                                }
+                                break;
+                            case "quit":
+                                {
+                                    ConsoleOutputForInput.QuitTheGame(quoridorController.QuoridorModel.CurrentPlayer);
+                                    quoridorController.QuoridorModel.IsGameEnded = true;
+                                    break;
+                                }
+                            default:
+                                ConsoleOutputForInput.UnknownCommand();
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ConsoleOutputForInput.Exception(ex.Message);
+                    }
+                }
+                else
+                {
+                    ConsoleOutputForInput.AITurn(quoridorController.QuoridorModel.CurrentPlayer);
+                    while(!isPlayerTurn)
+                    {
+                        try
+                        {
+                            weakAIController.MakeMove();
+                            isPlayerTurn = true;
+                        }
+                        catch
+                        {
+                            ConsoleOutputForInput.AIMistake(quoridorController.QuoridorModel.CurrentPlayer);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Quoridor.Console.App/ConsoleOutputForInput.cs
@@ -0,0 +1,57 @@
+﻿using Quoridor.Models.Interfaces;
+
+namespace Quoridor.Console.App
+{
+    class ConsoleOutputForInput
+    {
+        public static void OutputGameTitle()
+        {
+            System.Console.WriteLine("Welcome to greatest Quoridor game!");
+            System.Console.WriteLine("\nSelect type of game:");
+            System.Console.WriteLine("1. Player versus player (pvp)");
+            System.Console.WriteLine("2. Player versus artificial intelligence (pvai)");
+            System.Console.WriteLine("\nType 'pvp' or 'pvai' to select\n");
+        }
+        public static void OutputPvPModeTitle()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Quoridor PvP mode\n");
+        }
+        public static void OutputPvAIModeTitle()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Quoridor PvAI mode\n");
+        }
+
+        public static void InputView()
+        {
+            System.Console.Write("> ");
+        }
+        public static void PlayerInputView(IPlayer player)
+        {
+            System.Console.Write(player.Name + " > ");
+        }
+        public static void AITurn(IPlayer player)
+        {
+            System.Console.WriteLine($"{player.Name} thinks how to move...");
+            System.Threading.Thread.Sleep(1000);
+        }
+        public static void AIMistake(IPlayer player)
+        {
+            System.Console.WriteLine($"{player.Name} made a mistake and right now she will try to make a move again...");
+        }
+
+        public static void UnknownCommand()
+        {
+            System.Console.WriteLine($"\nUnknown command. Please try again...\n");
+        }
+        public static void Exception(string exceptionMassage)
+        {
+            System.Console.WriteLine($"\n{exceptionMassage}\nPlease try again...\n");
+        }
+        public static void QuitTheGame(IPlayer player)
+        {
+            System.Console.WriteLine($"\nPlayer {player.Mark} is out of the game. The game is over...\n");
+        }
+    }
+}
