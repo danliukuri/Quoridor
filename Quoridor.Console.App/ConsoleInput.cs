f031669ecbcb407d5134759815734ac06be16792
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-10-10 20:51:41 +0300

    Remove exceptions from the input system Refactor the input system

--- Quoridor.Console.App/ConsoleInput.cs
+++ Quoridor.Console.App/ConsoleInput.cs
@@ -1,6 +1,6 @@
 ﻿using Quoridor.Controllers;
+using Quoridor.ErrorHandling;
 using Quoridor.Models;
-using System;
 
 namespace Quoridor.Console.App
 {
@@ -44,87 +44,21 @@ namespace Quoridor.Console.App
 
         public void StartProcessingPvP(QuoridorController quoridorController, PlayerController playerController)
         {
-            string command;
             ConsoleOutputForInput.OutputPvPModeTitle();
             quoridorController.StartGame();
-            
+
             while (!quoridorController.QuoridorModel.IsGameEnded)
             {
-                ConsoleOutputForInput.PlayerInputView(quoridorController.QuoridorModel.CurrentPlayer);
-                command = System.Console.ReadLine();
-                string[] splitCommand = command.Split(new char[0]);
-                try
-                {
-                    switch (splitCommand[0].ToLower())
-                    {
-                        case "move":
-                            {
-                                Direction direction = splitCommand[1].ToLower() switch
-                                {
-                                    "up" => Direction.Up,
-                                    "down" => Direction.Down,
-                                    "left" => Direction.Left,
-                                    "right" => Direction.Right,
-                                    _ => throw new ArgumentException($"There is no such direction ({splitCommand[1]})")
-                                };
-                                if (splitCommand.Length == 3)
-                                {
-                                    Direction directionFromAnotherPlayer = splitCommand[2].ToLower() switch
-                                    {
-                                        "up" => Direction.Up,
-                                        "down" => Direction.Down,
-                                        "left" => Direction.Left,
-                                        "right" => Direction.Right,
-                                        _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
-                                    };
-                                    playerController.MakePlayerMove(direction, directionFromAnotherPlayer);
-                                }
-                                else
-                                    playerController.MakePlayerMove(direction);
-                            }
-                            break;
-                        case "place":
-                            {
-                                if (splitCommand[1].ToLower() != "wall")
-                                {
-                                    throw new ArgumentException($"\nUnknown command. Can't place '{splitCommand[1].ToLower()}'");
-                                }
-
-                                Direction direction = splitCommand[2].ToLower() switch
-                                {
-                                    "up" => Direction.Up,
-                                    "down" => Direction.Down,
-                                    "left" => Direction.Left,
-                                    "right" => Direction.Right,
-                                    _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
-                                };
-                                int w = int.Parse(splitCommand[3]) - 1;
-                                int h = int.Parse(splitCommand[4]) - 1;
-                                playerController.PlaceWall(direction, w, h);
-                            }
-                            break;
-                        case "quit":
-                            {
-                                ConsoleOutputForInput.QuitTheGame(quoridorController.QuoridorModel.CurrentPlayer);
-                                quoridorController.QuoridorModel.IsGameEnded = true;
-                                break;
-                            }
-                        default:
-                            ConsoleOutputForInput.UnknownCommand();
-                            break;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    ConsoleOutputForInput.Exception(ex.Message);
-                }
+                EitherLeftOrRight<ValidationError, bool> result =
+                    HandlePlayerTurn(quoridorController, playerController);
+                if(result.IsLeft)
+                     ConsoleOutputForInput.Exception(result.LeftOrDefault().Message);
             }
         }
 
         public void StartProcessingPvAI(QuoridorController quoridorController, PlayerController playerController,
             WeakAIController weakAIController)
         {
-            string command;
             ConsoleOutputForInput.OutputPvAIModeTitle();
             quoridorController.StartGame();
 
@@ -133,94 +67,112 @@ namespace Quoridor.Console.App
             { 
                 if (isPlayerTurn)
                 {
-                    ConsoleOutputForInput.PlayerInputView(quoridorController.QuoridorModel.CurrentPlayer);
-                    command = System.Console.ReadLine();
-                    string[] splitCommand = command.Split(new char[0]);
-                    try
-                    {
-                        switch (splitCommand[0].ToLower())
-                        {
-                            case "move":
-                                {
-                                    Direction direction = splitCommand[1].ToLower() switch
-                                    {
-                                        "up" => Direction.Up,
-                                        "down" => Direction.Down,
-                                        "left" => Direction.Left,
-                                        "right" => Direction.Right,
-                                        _ => throw new ArgumentException($"There is no such direction ({splitCommand[1]})")
-                                    };
-                                    if (splitCommand.Length == 3)
-                                    {
-                                        Direction directionFromAnotherPlayer = splitCommand[2].ToLower() switch
-                                        {
-                                            "up" => Direction.Up,
-                                            "down" => Direction.Down,
-                                            "left" => Direction.Left,
-                                            "right" => Direction.Right,
-                                            _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
-                                        };
-                                        playerController.MakePlayerMove(direction, directionFromAnotherPlayer);
-                                    }
-                                    else
-                                        playerController.MakePlayerMove(direction);
-                                    isPlayerTurn = false;
-                                }
-                                break;
-                            case "place":
-                                {
-                                    if (splitCommand[1].ToLower() != "wall")
-                                    {
-                                        throw new ArgumentException($"\nUnknown command. Can't place '{splitCommand[1].ToLower()}'");
-                                    }
-
-                                    Direction direction = splitCommand[2].ToLower() switch
-                                    {
-                                        "up" => Direction.Up,
-                                        "down" => Direction.Down,
-                                        "left" => Direction.Left,
-                                        "right" => Direction.Right,
-                                        _ => throw new ArgumentException($"There is no such direction ({splitCommand[2]})")
-                                    };
-                                    int w = int.Parse(splitCommand[3]) - 1;
-                                    int h = int.Parse(splitCommand[4]) - 1;
-                                    playerController.PlaceWall(direction, w, h);
-                                    isPlayerTurn = false;
-                                }
-                                break;
-                            case "quit":
-                                {
-                                    ConsoleOutputForInput.QuitTheGame(quoridorController.QuoridorModel.CurrentPlayer);
-                                    quoridorController.QuoridorModel.IsGameEnded = true;
-                                    break;
-                                }
-                            default:
-                                ConsoleOutputForInput.UnknownCommand();
-                                break;
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        ConsoleOutputForInput.Exception(ex.Message);
-                    }
+                    EitherLeftOrRight<ValidationError, bool> result =
+                        HandlePlayerTurn(quoridorController, playerController);
+                    if (result.IsLeft)
+                        ConsoleOutputForInput.Exception(result.LeftOrDefault().Message);
+                    else
+                        isPlayerTurn = result.RightOrDefault();
                 }
                 else
                 {
                     ConsoleOutputForInput.AITurn(quoridorController.QuoridorModel.CurrentPlayer);
-                    while(!isPlayerTurn)
-                    {
-                        try
-                        {
-                            weakAIController.MakeMove();
-                            isPlayerTurn = true;
-                        }
-                        catch
-                        {
-                            ConsoleOutputForInput.AIMistake(quoridorController.QuoridorModel.CurrentPlayer);
-                        }
-                    }
+                    weakAIController.MakeMove();
+                    isPlayerTurn = true;
                 }
             }
+        }
+
+        EitherLeftOrRight<ValidationError, bool> HandlePlayerTurn
+            (QuoridorController quoridorController, PlayerController playerController)
+        {
+            bool isPlayerTurn = true;
+            ConsoleOutputForInput.PlayerInputView(quoridorController.QuoridorModel.CurrentPlayer);
+            string command = System.Console.ReadLine();
+            string[] splitCommand = command.Split(new char[0]);
+
+            switch (splitCommand[0].ToLower())
+            {
+                case "move":
+                    {
+                        Direction direction;
+                        EitherLeftOrRight<ValidationError, Direction> directionResult =
+                            splitCommand[1].ParseToDirection();
+                        if (directionResult.IsLeft)
+                            return directionResult.LeftOrDefault();
+                        else
+                            direction = directionResult.RightOrDefault();
+
+                        EitherLeftOrVoid<ValidationError> result;
+                        if (splitCommand.Length == 3)
+                        {
+                            Direction directionFromAnotherPlayer;
+                            EitherLeftOrRight<ValidationError, Direction> directionFromAnotherPlayerResult =
+                                splitCommand[2].ParseToDirection();
+                            if (directionResult.IsLeft)
+                                return directionResult.LeftOrDefault();
+                            else
+                                directionFromAnotherPlayer = directionResult.RightOrDefault();
+
+                            result = playerController.TryToMakePlayerMove(direction, directionFromAnotherPlayer);
+                        }
+                        else
+                            result = playerController.TryToMakePlayerMove(direction);
+                        if (result.IsLeft)
+                            ConsoleOutputForInput.Exception(result.LeftOrDefault().Message);
+                        else
+                            isPlayerTurn = false;
+                    }
+                    break;
+                case "place":
+                    {
+                        if (splitCommand[1].ToLower() != "wall")
+                        {
+                            return new ValidationError($"\nUnknown command. Can't place '{splitCommand[1].ToLower()}'");
+                        }
+                        EitherLeftOrRight<ValidationError, Direction> directionResult =
+                            splitCommand[2].ParseToDirection();
+                        if (directionResult.IsLeft)
+                            return directionResult.LeftOrDefault();
+                        else
+                        {
+                            Direction direction = directionResult.RightOrDefault();
+
+                            int w;
+                            int h;
+                            EitherLeftOrRight<ValidationError, int> intResult = splitCommand[3].ParseToInt();
+                            if (intResult.IsLeft)
+                                return intResult.LeftOrDefault();
+                            else
+                                w = intResult.RightOrDefault() - 1;
+
+                            intResult = splitCommand[4].ParseToInt();
+                            if (intResult.IsLeft)
+                                return intResult.LeftOrDefault();
+                            else
+                                h = intResult.RightOrDefault() - 1;
+
+
+                            EitherLeftOrVoid<ValidationError> result =
+                                playerController.TryToPlaceWall(direction, w, h);
+                            if (result.IsLeft)
+                                ConsoleOutputForInput.Exception(result.LeftOrDefault().Message);
+                            else
+                                isPlayerTurn = false;
+                        }
+                    }
+                    break;
+                case "quit":
+                    {
+                        ConsoleOutputForInput.QuitTheGame(quoridorController.QuoridorModel.CurrentPlayer);
+                        quoridorController.QuoridorModel.IsGameEnded = true;
+                    }
+                    break;
+                default:
+                    ConsoleOutputForInput.UnknownCommand();
+                    break;
+            }
+            return isPlayerTurn;
         }
     }
 }
---
+++ Quoridor.Console.App/StringParsingExtentions.cs
@@ -0,0 +1,37 @@
+﻿using Quoridor.ErrorHandling;
+using Quoridor.Models;
+
+namespace Quoridor.Console.App
+{
+    static class StringParsingExtentions
+    {
+        public static EitherLeftOrRight<ValidationError, Direction> ParseToDirection(this string str)
+        {
+            Direction direction;
+            switch (str.ToLower())
+            {
+                case "up":
+                    direction = Direction.Up;
+                    break;
+                case "down":
+                    direction = Direction.Down;
+                    break;
+                case "left":
+                    direction = Direction.Left;
+                    break;
+                case "right":
+                    direction = Direction.Right;
+                    break;
+                default:
+                    return new ValidationError($"There is no such direction ({str})");
+            };
+            return direction;
+        }
+        public static EitherLeftOrRight<ValidationError, int> ParseToInt(this string str)
+        {
+            if (!int.TryParse(str, out int result))
+                return new ValidationError($"Can't parse '{str}' to int value");
+            return result;
+        }
+    }
+}
