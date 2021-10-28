70921abe0d0254e297c57590adde834b3e3320b9
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-10-29 00:05:38 +0300

    Create the simplest function for calculating SEV

---
+++ Quoridor.Controllers/PlayerControllers/AI/Strong/MinimaxAdapters/MinimaxScoreFunctionForTwoPlayers.cs
@@ -0,0 +1,42 @@
+﻿using Quoridor.Models;
+using System;
+
+namespace Quoridor.Controllers.PlayerControllers.AI.Strong.MinimaxAdapters
+{
+    public static class MinimaxScoreFunction
+    {
+        public static double CalculateForTwoPlayers(MinimaxStateAdapter minimaxAdapter, Player player)
+        {
+            if (minimaxAdapter is null)
+            {
+                throw new ArgumentNullException(nameof(minimaxAdapter));
+            }
+            if (player is null)
+            {
+                throw new ArgumentNullException(nameof(player));
+            }
+
+            QuoridorModel quoridorModel = minimaxAdapter.QuoridorModel;
+            if (quoridorModel.Players.Length != 2)
+            {
+                throw new InvalidOperationException($"There must be two players in the game." +
+                    " Parameter name: " + nameof(quoridorModel.Players));
+            }
+
+            Player firstPlayer = (Player)quoridorModel.Players[0];
+            Player secondPlayer = (Player)quoridorModel.Players[1];
+            double score; // A number that shows how close the player is to victory
+
+            if (player == firstPlayer)
+            {
+                // Value inversely proportional to the number of steps to victory
+                score = (double)(quoridorModel.Field.Height - 1) / firstPlayer.Goal.Item2(firstPlayer.Position);
+            }
+            else
+            {
+                score = (double)(quoridorModel.Field.Height - 1) / secondPlayer.Goal.Item2(secondPlayer.Position);
+            }
+            return score;
+        }
+    }
+}
